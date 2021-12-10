efbb7bda8e9a5db44a9cd720fef571279dc65ba6
Ankita Bagul <88926647+ankitabagul8@users.noreply.github.com>
2021-12-10 17:14:36 +0530

    Add files via upload

---
+++ feedback-from.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-from',
+  templateUrl: './feedback-from.component.html',
+  styleUrls: ['./feedback-from.component.css']
+})
+export class FeedbackFromComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
